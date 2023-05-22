c3e5f869a27274a245e4f65d5c7706a736d29b70
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-22 16:37:36 +0200

    created function

---
+++ myscript.js
@@ -0,0 +1,15 @@
+//* Palidroma
+//* Chiedere all’utente di inserire una parola
+//* Creare una funzione per capire se la parola inserita è palindroma
+
+const usertWord = prompt('Hi! Enter the first word');
+
+
+
+function wordCheck (word, polidromeWord){
+    if (word.value === polidromeWord.value){
+        return true;
+    }
+    return false;
+}
+
