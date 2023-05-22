225b8bed989f8c108142f54d93d750505fed403a
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-22 17:10:32 +0200

    added if and else

--- myscript.js
+++ myscript.js
@@ -3,8 +3,14 @@
 //* Creare una funzione per capire se la parola inserita è palindroma
 
 const usertWord = prompt('Hi! Enter the first word');
+const sameWord = ""
 
 
+if (usertWord.length === sameWord.length){
+    console.log('La parola è polidroma')
+} else{
+    console.log('Non è polidroma')
+}
 
 function wordCheck (word, polidromeWord){
     if (word.value === polidromeWord.value){
